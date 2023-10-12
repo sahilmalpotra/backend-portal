5818c4e2787320834871b7fafac6ac7bc1e02d62
sahilmalpotra <sahilmalpotra2007@gmail.com>
2023-10-12 17:15:01 +0530

    add comment

--- Investment Portal/ApiConstant.cs
+++ Investment Portal/ApiConstant.cs
@@ -5,6 +5,9 @@
 #pragma warning disable S2339 // Public constant members should not be used
         public const string ServiceName = "incinvest";
         public const string RouteSuffix = "Ip";
+        
 #pragma warning restore S2339 // Public constant members should not be used
     }
 }
+
+//creating pull request
