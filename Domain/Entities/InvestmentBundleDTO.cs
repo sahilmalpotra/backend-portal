a2ff000a24561cbfda58258a7235213ed64bf5ed
Ritika Agrawal <ritika.agrawal3@incedoinc.com>
2023-12-01 16:18:35 +0530

    New DTO added in Domain

---
+++ Domain/Entities/InvestmentBundleDTO.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class InvestmentBundleDTO
+    {
+        public string InvestmentId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public List<Strategy> Strategies { get; set; }
+
+    }
+}
