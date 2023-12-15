58e1018551515f24c672e4c38b934e2201cb02a1
priyanka290601 <paggarwal2906@gmail.com>
2023-12-15 12:48:53 +0530

    Revert "master table"
    This reverts commit 29442963eba5b1496600c972fa237fc0e223b448.

--- Domain/Interfaces/IStrategy.cs
+++ Domain/Interfaces/IStrategy.cs
@@ -17,11 +17,6 @@ namespace Domain.Interfaces
         Task<IEnumerable<Strategy>> GetStrategiesByClientIdAsync(string clientId);
         Task UpdateInvestmentAmountAsync(string strategyId, decimal newInvestmentAmount);
 
-        PastData GetCompany(string name);
-        Task<IEnumerable<StockData>> GetStockData(string companyName);
-        Task<bool> AddCompany(PastData pastData);
-        Task<bool> PostStockData(string companyName, StockData stockData);
-
         Task<IEnumerable<Strategy>> GetStrategiesByAdvisorIdAsync(string advisorId);
         Task<IEnumerable<Strategy>> GetApprovedStrategiesByAdvisorIdAsync(string advisorId);
         Task<string> AddStrategyAsync(Strategy strategy);
